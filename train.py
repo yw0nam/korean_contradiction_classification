d079290cbbca58dea453450633166b7a32451ed6
yw0nam <spow2544@gmail.com>
2022-02-08 15:43:46 +0900

    update cross validation

--- train.py
+++ train.py
@@ -5,10 +5,10 @@ import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from transformers import Trainer
 from transformers import TrainingArguments
-import argparse
+import argparse, os, json
 from datasets import load_metric
-import os
-import json
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold
 
 def define_argparser():
     p = argparse.ArgumentParser()
@@ -16,19 +16,18 @@ def define_argparser():
     p.add_argument('--model_fn', default='kykim/bert-kor-base')
     p.add_argument('--save_path', default='./model/')
     p.add_argument('--train_fn', required=True)
-    p.add_argument('--valid_fn', default=None)
     p.add_argument('--gradient_accumulation_steps', type=int, default=2)
     p.add_argument('--batch_size_per_device', type=int, default=64)
     p.add_argument('--n_epochs', type=int, default=10)
     p.add_argument('--warmup_ratio', type=float, default=.2)
     p.add_argument('--max_length', type=int, default=512)
-    p.add_argument('--random_state', default=None, type=int)
+    p.add_argument('--random_state', default=512, type=int)
 
     config = p.parse_args()
 
     return config
     
-def train_model(config, train_dataset, valid_dataset):
+def train_model(config, train_dataset, valid_dataset, save_path):
 
     # You can change model here.
     tokenizer = AutoTokenizer.from_pretrained(config.model_fn)
@@ -49,16 +48,20 @@ def train_model(config, train_dataset, valid_dataset):
     )
 
     training_args = TrainingArguments(
-        output_dir=os.path.join(config.save_path, 'checkpoints'),
+        output_dir=os.path.join(save_path, 'checkpoints'),
         num_train_epochs=config.n_epochs,
         per_device_train_batch_size=config.batch_size_per_device,
         per_device_eval_batch_size=config.batch_size_per_device,
         warmup_steps=n_warmup_steps,
         weight_decay=0.01,
         fp16=True,
-        evaluation_strategy='steps',
-        logging_steps=n_total_iterations // 200,
-        save_strategy ='steps',
+        logging_strategy='epoch',
+        evaluation_strategy='epoch',
+        save_strategy='epoch',
+        metric_for_best_model='accuracy',
+        greater_is_better=True,
+        load_best_model_at_end=True,
+        label_smoothing_factor=0.0,
         save_steps=n_total_iterations // config.n_epochs,
         gradient_accumulation_steps=config.gradient_accumulation_steps,
         metric_for_best_model ="accuracy"
@@ -76,10 +79,9 @@ def train_model(config, train_dataset, valid_dataset):
     )
 
     trainer.train()
-    
-    trainer.model.save_pretrained(config.save_path)
+    trainer.model.save_pretrained(save_path)
 
-    with open(os.path.join(config.save_path, 'args.json'), 'w') as f:
+    with open(os.path.join(save_path, 'args.json'), 'w') as f:
         json.dump(config.__dict__, f, indent=2)
         
 def compute_metrics(eval_pred):
@@ -90,11 +92,15 @@ def compute_metrics(eval_pred):
 
 if __name__ == '__main__':
     config = define_argparser()
+    skf = StratifiedKFold(n_splits=5, random_state=512, shuffle=True)
+    data = pd.read_csv('./data/csv.tsv', sep='\t', names=['input_seq', 'label'])
 
-    if config.valid_fn:
-        train_dataset = get_datasets(config.train_fn)
-        valid_dataset = get_datasets(config.valid_fn)
-    else:
-        train_dataset, valid_dataset = get_datasets(config.train_fn, config.random_state)
+    for cv_idx, data_index in enumerate(skf.split(data['input_seq'], data['label'])):
+        train_index, val_index = data_index[0], data_index[1]
+        train_seq, val_seq = data['input_seq'][train_index], data['input_seq'][val_index]
+        train_label, val_label = data['label'][train_index], data['label'][val_index]
         
-    train_model(config, train_dataset, valid_dataset)
+        train_dataset = load_Dataset(train_seq.to_list(), train_label.to_list())
+        valid_dataset = load_Dataset(val_seq.to_list(), val_label.to_list())
+        save_path = os.path.join(config.save_path, str(cv_idx))
+        train_model(config, train_dataset, valid_dataset, save_path)
--- utils.py
+++ utils.py
@@ -54,37 +54,3 @@ class load_Dataset(Dataset):
             'input_seq': input_seq,
             'label': label,
         }
-
-def get_datasets(fn, random_state=None):
-    with open(fn, 'r') as f:
-        lines = f.readlines()
-
-        input_seqs, labels = [], []
-        for line in lines:
-            if line.strip() != '':
-                # The file should have tab delimited two columns.
-                # First column indicates label field,
-                # and second column indicates text field.
-                input_seq, label = line.strip().split('\t')
-                input_seqs += [input_seq]
-                labels += [label]
-    if random_state:
-        train, val = train_test_split(list(zip(input_seqs, labels)), 
-                                        test_size=0.1, 
-                                        stratify=labels, 
-                                        random_state=random_state)
-
-        train_seq = [e[0] for e in train]
-        train_label = [e[1] for e in train]
-
-        val_seq = [e[0] for e in val]
-        val_label = [e[1] for e in val]
-
-        train_dataset = load_Dataset(train_seq, train_label)
-        val_dataset = load_Dataset(val_seq, val_label)
-        return train_dataset, val_dataset
-    else:
-        dataset = load_Dataset(input_seq, labels)
-        return dataset
-
-    
